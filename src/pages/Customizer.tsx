708bb57999054d8dae402c3958ed39aba7fa5b99
atolaini <adam.tolaini@gmail.com>
2023-10-20 18:58:02 +0100

    working on basic layout

--- src/pages/Customizer.tsx
+++ src/pages/Customizer.tsx
@@ -9,8 +9,70 @@ import { downloadCanvasToImage, reader } from '../config/helpers';
 import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
 import { fadeAnimation, slideAnimation } from '../config/motion';
 
+import {
+  AIPicker,
+  ColorPicker,
+  CustomButton,
+  FilePicker,
+  Tab,
+} from '../components';
+
 const Customizer = () => {
-  return <div>Customizer</div>;
+  const snap = useSnapshot(state);
+
+  const goBack = () => {
+    state.intro = true;
+  };
+  return (
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key='custom'
+            className='absolute top-0 left-0 z-10'
+            {...slideAnimation('left')}
+          >
+            <div className='flex items-center min-h-screen'>
+              <div className='editortabs-container tabs'>
+                {EditorTabs.map((tab) => (
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick
+                  />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+          <motion.div
+            className='absolute z-10 top-5 right-5'
+            {...fadeAnimation}
+          >
+            <CustomButton
+              styleType='filled'
+              title='Go Back'
+              handleClick={goBack}
+              customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+            />
+          </motion.div>
+          <motion.div
+            className='filtertabs-container'
+            {...slideAnimation('up')}
+          >
+            {FilterTabs.map((tab) => (
+              <Tab
+                key={tab.name}
+                tab={tab}
+                isFilterTab
+                isActiveTab=''
+                handleClick={() => {}}
+              />
+            ))}
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Customizer;
